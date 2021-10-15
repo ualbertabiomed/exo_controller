5462daa280b79ad16b976593b97e93638aece6e6
c-m-w <c-m-w-other@tuta.io>
2021-10-14 22:40:31 -0600

    update ALEXNode.py

--- include/ALEXNode.py
+++ include/ALEXNode.py
@@ -1,6 +1,61 @@
 import abc
+import rospy
 
-class ALEXNode(abc):
+"""
+    base class for all ALEX ROS nodes
+"""
+class ALEXNode(abc, metaclass=abc.ABCMeta):
 
-    def __init__(self):
+    # one callback per node?
+
+    """
+        initialize ROS node
+
+        name: name of the ROS (ALEX) node
+    """
+    def __init__(self, name):
+        self.name  = name # name can possibly be used for logging later
+        publishers = {}
+
+        rospy.init_node(name)
+
+    """
+        subscribe to a topic
+
+        topic:    the topic to subscribe to
+        callback: the callback to subscribe with
+    """
+    def subscribe(self, topic, callback):
+        rospy.Subscriber(topic, String, callback)
+
+    """
+        create a publisher
+
+        topic: the topic to which the new publisher will publish
+    """
+    def createPublisher(self, topic):
+        if not topic in publishers
+            publishers[topic] = rospy.Publisher(topic, String)
+
+    """
+        publish a message
+
+        topic:   the topic to publish to
+        message: the message to be published
+    """
+    def publish(self, topic, message):
+        if topic in publishers:
+            publishers[topic].publish(message)
+
+    def log(self):
         pass
+
+    """
+        get the status of the ALEXNode
+
+        to be defined by child classes
+    """
+    @abstractmethod
+    def status(self):
+        raise NotImplementedError
+
