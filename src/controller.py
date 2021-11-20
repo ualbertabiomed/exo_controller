7fedba43307814a8f3af7f0e5f95bda95521f14f
Justin Boileau <45305362+jboileau99@users.noreply.github.com>
2021-11-19 17:50:03 -0700

    Update controller.py

--- src/controller.py
+++ src/controller.py
@@ -3,4 +3,9 @@
 Main node that gets data, does calculations, sends commands to ODrive/Motor, brake mechanism, terminal, etc.
 """
 
-import 
+import ALEXRos
+import rospy
+
+class ControllerNode(AlexRos):
+  
+  
